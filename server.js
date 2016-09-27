e9ef650b439b3f4b5935c42c9e97ee3230157aa3
IpshitaP <ipipau.123@gmail.com>
2016-09-28 03:00:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var hhtp = require("http");
 
 var app = express();
 app.use(morgan('combined'));
@@ -16,6 +17,19 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
+http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
