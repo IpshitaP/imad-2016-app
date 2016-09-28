2b553559ecd9bc1f024b7c38592730a2fbe21c7d
IpshitaP <ipipau.123@gmail.com>
2016-09-28 07:58:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var hhtp = require("http");
+var hhtp = require('http');
 
 var app = express();
 app.use(morgan('combined'));
